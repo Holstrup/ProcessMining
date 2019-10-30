0a8b41507d2a1125e3c197c588c75880bd301ca6
lassestarklit <s165498@student.dtu.dk>
2019-10-30 15:05:43 +0100

    initial nlp commit

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
-from 
+import Data_Processing_Transformation
+from Natural_Language_Processing import  NLP
 
 if __name__ == '__main__':
-    Data_Processing_Transformation.Data_Processing_Transformation("Test")
+
+    #Data_Processing_Transformation.Data_Processing_Transformation("Test")
+    nlp=NLP()
+    nlp.set_sentence("Hello. How are you doing?")
+    nlp.chance_of_question_sentence()
+
---
+++ Natural_Language_Processing.py
@@ -0,0 +1,17 @@
+import nltk
+
+class NLP:
+
+    def __init__(self):
+        posts = nltk.corpus.nps_chat.xml_posts()[:10000]
+        for post in posts:
+
+            print (post.text)
+    def set_sentence(self,text):
+        self.sentence=text
+
+    def tokenize(self):
+        return nltk.word_tokenize(self.sentence)
+
+    #def is_question(self):
+   

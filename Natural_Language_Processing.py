da49c5e0e78609d35e471d80f4b9e4d8c20210b1
lassestarklit <s165498@student.dtu.dk>
2019-10-30 16:57:14 +0100

    initial nlp commit

--- Natural_Language_Processing.py
+++ Natural_Language_Processing.py
@@ -2,15 +2,20 @@ import nltk
 
 class NLP:
 
-    def __init__(self):
-        posts = nltk.corpus.nps_chat.xml_posts()[:10000]
-        for post in posts:
-
-            print (post.text)
     def set_sentence(self,text):
         self.sentence=text
 
     def tokenize(self):
         return nltk.word_tokenize(self.sentence)
 
-    #def is_question(self):
+    def is_question(self):
+        '''Here goes logic'''
+        if "?" in self.tokenize():
+            return True
+        else:
+            return False
+
+    def get_tags(self):
+        tokens=self.tokenize()
+
+        print(nltk.pos_tag(tokens))

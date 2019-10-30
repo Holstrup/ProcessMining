ac56bfc852214cdd70728a5d6722469aea9d23eb
lassestarklit <s165498@student.dtu.dk>
2019-10-30 16:58:26 +0100

    initial nlp commit

--- Main.py
+++ Main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     #Data_Processing_Transformation.Data_Processing_Transformation("Test")
     nlp=NLP()
     nlp.set_sentence("Hello. How are you doing?")
-    nlp.chance_of_question_sentence()
+    print(nlp.get_tags())
 

b5d44a1276e3a895becf98dc3e47f059a29ee437
Holstrup <abho@dtu.dk>
2019-11-12 15:18:59 +0100

    Worked with kaggle dataset and set up pipeline for extracting data

--- Data_Processing_Transformation.py
+++ Data_Processing_Transformation.py
@@ -6,17 +6,75 @@ from pm4py.objects.log.exporter.xes import factory as xes_exporter
 from pm4py.objects.log.importer.xes import factory as xes_import_factory
 from pm4py.objects.log.util.log import log as pmlog
 from Natural_Language_Processing import NLP
+import pandas as pd
+from datetime import datetime
 
 class Data_Processing_Transformation:
-    def __init__(self, path_name):
+    def __init__(self, path_name, social_graph):
         self.path_name = path_name             # Slack filename
         self.log_file_name = "SlackDataLog.xes"
+        self.social_graph = social_graph
 
         # Run Pipeline
-        self.extraction()
-        log = self.transformation()
-        xes_exporter.export_log(log, self.log_file_name)
+        log = self.kaggle_extract()
+        #xes_exporter.export_log(log, "GitterLogSocial.xes")
 
+
+        #self.extraction()
+        #log = self.transformation()
+        #xes_exporter.export_log(log, self.log_file_name)
+
+
+
+    def kaggle_extract(self):
+        chunksize = 10 ** 3
+        nlp_class = NLP()
+        last_date = datetime.strptime("2015-02-01T00:00:00.000Z", '%Y-%m-%dT%H:%M:%S.%fZ')
+        log = pmlog.EventLog()
+        trace = pmlog.Trace()
+
+        break_loop = False
+        for chunk in pd.read_csv(self.path_name, chunksize=chunksize, usecols = ["id", "fromUser.displayName", "text", "sent"]):
+            if break_loop:
+                break
+
+            for index, row in chunk.iterrows():
+                nlp_class.set_sentence(row['text'])
+                date_string = row["sent"]
+                datetime_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+                if row["fromUser.displayName"] == "nan":
+                    print(index)
+
+                if index == 0:
+                    prev_date = datetime_object.date()
+                elif datetime_object.date() > prev_date:
+                    prev_date = datetime_object.date()
+                    log.append(trace)
+                    trace = pmlog.Trace()
+
+                if datetime_object > last_date:
+                    break_loop = True
+                    break
+
+                try:
+                    case_dict = {}
+                    #case_dict["org:resource"] = row["fromUser.displayName"]
+                    case_dict["org:resource"] = str(nlp_class.get_class())
+                    #case_dict['concept:name'] = str(nlp_class.get_class())
+                    case_dict['concept:name'] = row["fromUser.displayName"]
+
+                    case_dict["time:timestamp"] = date_string
+
+                    event = pmlog.Event(case_dict)
+                    trace.append(event)
+
+                except TypeError:
+                    continue
+
+            print(row["sent"])
+        log.append(trace)
+        return log
 
 
 
@@ -58,9 +116,16 @@ class Data_Processing_Transformation:
 
                 try:
                     case_dict = {}
-                    case_dict["org:resource"] = message['user_profile']['real_name']
+
+                    if not self.social_graph:
+                        case_dict["org:resource"] = message['user_profile']['real_name']
+                        case_dict['concept:name'] = str(nlp_class.get_class())
+                    else:
+                        case_dict['concept:name']  = message['user_profile']['real_name']
+                        case_dict["org:resource"] = str(nlp_class.get_class())
+
+
                     case_dict["text"] = message['text']
-                    case_dict['concept:name'] = str(nlp_class.get_class())
                     case_dict["time:timestamp"] = message['ts']
 
 
@@ -70,20 +135,5 @@ class Data_Processing_Transformation:
                     print(message)
         return log
 
-
-
-
-
-
-    def classification(self, message):
-        """
-        This is where the magic happens. We need to take a message in
-        and output a classification based on the message
-
-        @:param message: Message String
-        @:return classifiction: Q, A or C
-        """
-
-        return random.choice(self.message_types)
 
 
